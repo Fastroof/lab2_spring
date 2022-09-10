83b2c622641127f769b4d496a8c6d6a007bdaeff
YuraShum <yura.shumel@gmail.com>
2022-09-10 13:37:42 +0300

    Виведення інформація про users. Ін'єкція залежностей через поле та додання атрибуту до моделі.

--- src/main/java/com/fastroof/lab2_spring/controller/IndexController.java
+++ src/main/java/com/fastroof/lab2_spring/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.fastroof.lab2_spring.controller;
 
+import com.fastroof.lab2_spring.repository.FakeUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private FakeUserRepository fakeUserRepository = new FakeUserRepository();
 
     @GetMapping("/")
-    public String showIndexPage() {
+    public String showIndexPage(ModelMap model) {
+        model.addAttribute("users", fakeUserRepository.getUsers());
         return "thymeleaf/index";
     }
 }
--- src/main/java/com/fastroof/lab2_spring/repository/FakeUserRepository.java
+++ src/main/java/com/fastroof/lab2_spring/repository/FakeUserRepository.java
@@ -1,6 +1,8 @@
 package com.fastroof.lab2_spring.repository;
 
 import com.fastroof.lab2_spring.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -8,14 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Profile("transient")
+@Getter
+@Setter
 public class FakeUserRepository implements UserRepository {
-
-    List<User> users = new ArrayList<>();
-
+    private final List<User> users = new ArrayList<>();
     public FakeUserRepository() {
-        this.users.add(new User(0L, "john@gmail.com", "12345678", "John", "Bill"));
-        this.users.add(new User(1L, "bob@gmail.com", "87654321", "Bob", "Brown"));
+        users.add(new User(0L, "john@gmail.com", "12345678", "John", "Bill"));
+        users.add(new User(1L, "bob@gmail.com", "87654321", "Bob", "Brown"));
     }
 
 }
